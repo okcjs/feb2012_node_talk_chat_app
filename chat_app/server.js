cb1e9e2700216ec7dd1d8a003a9b1033ddd76679
Ben Van Treese <vantreeseba@gmail.com>
2012-02-18 15:09:52 -0600

    starting on socket.io server, but no client side code

--- chat_app/server.js
+++ chat_app/server.js
@@ -1,5 +1,15 @@
 var connect = require("connect");
+var io = require("socket.io");
 
 connect(connect.static(__dirname + "/public")).listen(8000, function(){
 	console.log("static server listening on port 8000");
+});
+
+
+var ioServer = io.listen(1337,function(){
+	console.log("socket server listening on port 1337");
+});
+
+ioServer.sockets.on("connection", function(socket){
+	console.log("someone connected");
 });
